ab02e427d3899599dbdbd6e2ee2ce153172a786f
Justin Pacquing <pacqqu@gmail.com>
2015-12-10 18:50:19 -0500

    slideshow works

--- static/imgine.js
+++ static/imgine.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-	$.get("/image", function(data){
+	
+	var imgnum = 2;
+	
+	function getImage(i) {
+	$.get("/image/" + i, function(data){
 		console.log("What");
 		console.log(data);
 		$("#slideimg").attr("src",data);
 	    });
+	};
+
+	var slideshow = function slideshow(){
+	    getImage(imgnum);
+	    if (imgnum < 5)
+		imgnum++;
+	    else
+		imgnum = 1;
+	};
+	
+	setInterval(slideshow,2000);
     });
